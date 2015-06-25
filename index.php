be7725fb7617e171a9ab31b719cde0d3f38ca377
Mehmet Dik <mdik1212@gmail.com>
2015-06-25 17:49:19 +0300

    Code Design

--- index.php
+++ index.php
@@ -2,45 +2,49 @@
 include("connect.php");
 
 
-if (isset($_POST['caller'])) {
-	
+if (isset($_POST['caller'])) 
+{
 	$caller=$_POST['caller'];
+	$qry=mysql_query("select * from information where phone=$caller");
 	
-$qry=mysql_query("select * from information where phone=$caller");
-	
-	
-	if ($qry) {
-		while ($take=mysql_fetch_array($qry)) {
+	if ($qry) 
+	{
+		while ($take=mysql_fetch_array($qry)) 
+		{
 			$name=$take['name'];
 			$surname=$take['surname'];
 			$setname="$name"." "."$surname";
-			}
+		}
 
 		$value=array(
+				
 			"bfxm" => array("version"=>1),
 			"seq"=> array(
-				array(
-					"action"=>"set_caller_name",
-					"args"=>array(
-						"caller_name" =>"$setname"
-					)
-					)
-					
 						
-				)
-			);
+						array(
+							"action"=>"set_caller_name",
+							"args"=>array("caller_name" =>"$setname")
+							 )
+											
+				         )
+					);
 	}else{
+			
 		$value=array(
+				
 			"bfxm" => array("version"=>1),
 			"seq"=> array(
-				array(
-					"action"=>"dial",
-					"args" => ["destination"=> 10]	
-					)
-				)
-			);
+						
+						array(
+							"action"=>"dial",
+							"args" => ["destination"=> 10]	
+							 )
+						
+						  )
+				
+					);
 
-	}
+		 }
 	
 
 
