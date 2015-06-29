e502beb8fcd0e018fd0cbb216ad5fd08538f8bf1
Mehmet Dik <mdik1212@gmail.com>
2015-06-29 11:34:33 +0300

    Php Kod Standart Fixer

--- index.php
+++ index.php
@@ -2,55 +2,63 @@
 include("connect.php");
 
 
-if (isset($_POST['caller'])) 
+if (isset($_POST['caller']))
 {
-	$caller=$_POST['caller'];
-	$qry=mysql_query("select * from information where phone=$caller");
-	
-	if ($qry) 
-	{
-		while ($take=mysql_fetch_array($qry)) 
-		{
-			$name=$take['name'];
-			$surname=$take['surname'];
-			$setname="$name"." "."$surname";
-		}
-
-		$value=array(
-				
-			"bfxm" => array("version"=>1),
-			"seq"=> array(
-						
-					array(
-						"action"=>"set_caller_name",
-						"args"=>array("caller_name" =>"$setname")
-					     )	
-											
-				      )
-			     );
-	}else{
-			
-		$value=array(
-				
-			"bfxm" => array("version"=>1),
-			"seq"=> array(
-						
-					array(
-						"action"=>"dial",
-						"args" => ["destination"=> 10]	
-					     )
-						
-				     )
-				
-			     );
-
-	     }
-	
-
-
-	header('Content-Type: application/json');
-	die(json_encode($value));
-	
+    $caller=$_POST['caller'];
+    $qry=Query("select * from information where phone=$caller");
+    
+    if ($qry)
+    {
+        $setname=Setname($qry);
+        
+        $value=array(
+        
+        "bfxm" => array("version"=>1),
+        "seq"=> array(
+        "action"=>"set_caller_name",
+        "args"=>array("caller_name" =>"$setname")
+        )
+        );
+        }else{
+        
+        $value=array(
+        
+        "bfxm" => array("version"=>1),
+        "seq"=> array(
+        "action"=>"dial",
+        "args" => ["destination"=> 10]
+        )
+        );
+    }
+    
+    
+    
+    header('Content-Type: application/json');
+    die(json_encode($value));
+    
 }
+
+function Query ($sorgu)
+{
+    $str = mysql_query($sorgu);
+    return $str;
+}
+
+function Fetch ($sorgu)
+{
+    $str = mysql_fetch_array($sorgu);
+    return $str;
+}
+
+function Setname ($qry)
+{
+    while ($take=Fetch($qry))
+    {
+        $setname=$take['name']." ".$take['surname'];
+    }
+    
+    return $setname;
+}
+
 
 ?>
